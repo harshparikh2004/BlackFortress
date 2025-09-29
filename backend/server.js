95c35a63793a2f1df94574261c513a0a9404c87f
shadow4114 <125117032+shadow4114@users.noreply.github.com>
2025-09-30 03:07:20 +0530

    minor bug fixes

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,25 @@ app.use(helmet({
     contentSecurityPolicy: {
         directives: {
             defaultSrc: ["'self'"],
-            styleSrc: ["'self'", "'unsafe-inline'"],
-            scriptSrc: ["'self'"],
+            styleSrc: [
+                "'self'",
+                "'unsafe-inline'",
+                'https://fonts.googleapis.com',
+                'https://cdnjs.cloudflare.com'
+            ],
+            fontSrc: [
+                "'self'",
+                'https://fonts.gstatic.com',
+                'https://cdnjs.cloudflare.com'
+            ],
+            scriptSrc: ["'self'", "'unsafe-inline'", 'https://cdnjs.cloudflare.com'],
             imgSrc: ["'self'", "data:", "https:"],
+            connectSrc: [
+                "'self'",
+                process.env.NODE_ENV === 'production'
+                    ? 'https://black-fortress.vercel.app/'
+                    : 'http://localhost:5000'
+            ]
         },
     },
 }));
